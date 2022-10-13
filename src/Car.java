4c8da3b81d56a057263a807febcf7ba1c23153b5
Abdulov Ruslan <you@example.com>
2022-10-13 18:02:36 +0400

    first commit

---
+++ src/Car.java
@@ -0,0 +1,22 @@
+public class Car {
+    /**
+     * @param - nothing
+     * @return - info for building
+     */
+    public static void build() {
+        System.out.println("You must build a new car");
+    }
+
+    /**
+     * @param - nothing
+     * @return - info for buying
+     */
+    public static void buy() {
+        System.out.println("I will buy this car");
+    }
+
+    /**
+     * @return - car speed
+     */
+    int speed = 0;
+}
---
+++ src/Main.java
@@ -0,0 +1,8 @@
+public class Main {
+    public static void main(String[] args) {
+        int a = 10;
+        int b = 25;
+        int c = 0;
+        System.out.println(a + b * 10 >= 255 && 4 << 1 <= 14) ;
+    }
+}
